2767d009c18105e0965718981ddb8a454dee5d91
SnrNotHere16 <aquineda16@hotmail.com>
2021-01-31 23:00:14 -0800

    Update main.c (Successfully connects to Processing)
    The following commit updates main.c. This successfully connects the tm4c123 onto processing. This is the intended main function structure of the sole's functionality.

--- Modules/SolePressurePad/PressurePadSole/main.c
+++ Modules/SolePressurePad/PressurePadSole/main.c
@@ -1,32 +1,14 @@
-//#include <stdio.h>
-//#include <stdint.h>
-//#include "tm4c123gh6pm.h"
-//#include "init.h"
-
-
-
-
-//int main (){
-
-//	while(1){
-//		
-//		
-//	}
-//}
-
-/*
-	Read data from UART0 and display 3 bits of it on the LEDs
-*/
-
-#include <stdint.h>
+/#include <stdint.h>
 #include "tm4c123gh6pm.h"
 #include "init.h"
 int32_t main(void){
- 
+ 	//initialize UART
 	initUART();
+	//Initialize pins for the sole
+	initSole();
 	while(1){
-		//whileUART();
-		UART0_TX( 250 );
+	//reads the values from the sole
+	whileUART();
 	}
 }
 
